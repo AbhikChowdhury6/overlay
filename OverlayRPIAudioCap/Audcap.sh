fa9d4852c698665abd504723f53813981bb26069
Abhik Chowdhury <Abhik.ch6@gmail.com>
2018-09-07 20:43:57 -0700

    made good bash scripts for camera and audio also added raw code for i2c sensors

--- OverlayRPIAudioCap/Audcap.sh
+++ OverlayRPIAudioCap/Audcap.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-raspivid -t 0 -n -w 1280 -h 720 -hf -ih -fl -fps 20 -o - | tee >test1.h264 | nc -k -l 2322
+t="$(date +"%d-%m-%Y-%H-%M-%S.%N")"
+productName=$2
+sensorName=$1
+name=$productName-$sensorName-$t-.wav
+echo $name
+arecord --rate 44100 -c2 $name
---
+++ Overlayi2cCap/i2cHatBackPiStreamManager.py
@@ -0,0 +1,47 @@
+# thisfile is to grab all of the data on the i2c bus based on the the configuration file
+#write it to files
+# keep an open socket and stream it (via jttp https later)
+import time
+import smbus
+import os
+
+#import all of the devices on the bus for now
+
+from libraries.i2cMPU92500 import MPU92500
+
+bus = smbus.SMBus(1)
+
+#defince product name
+PRODUCT_NAME = "AbhiksHappininHat"
+DATA_PATH = "AHH"
+
+
+# extanciate and configure all of the objects
+
+mpu92500 = MPU92500(bus, DATA_PATH)
+
+
+
+print time.strftime('%d-%m-%Y-%H-%M-%S', time.localtime())
+divider = 1
+second = 0
+starttime=time.time()
+while True:
+  #a bunch of if statementsfor each sensors
+  if (divider % ecg.REFRESH_RATE == 0):
+      ecg.log(bus)
+
+  if (divider % gsr.REFRESH_RATE == 0):
+      gsr.log(bus)
+
+  if (divider % breath.REFRESH_RATE == 0):
+      breath.log(bus)
+
+  if (divider % mpu60501.REFRESH_RATE == 0):
+      mpu60501.log(bus)
+
+  if (divider % mpu92500.REFRESH_RATE == 0):
+      mpu92500.log(bus)
+
+  divider = divider + 1
+  time.sleep(0.01 - ((time.time() - starttime) % 0.01))

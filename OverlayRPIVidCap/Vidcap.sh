835c8941d67bc936cda13266dc3ccb34f6df8cad
Abhik Chowdhury <abhik.ch6@gmail.com>
2018-09-07 21:10:10 -0700

    modified audio and video cap to run at runtime

--- OverlayRPIVidCap/Vidcap.sh
+++ OverlayRPIVidCap/Vidcap.sh
@@ -4,4 +4,4 @@ productName=$2
 sensorName=$1
 name=$productName-$sensorName-$t-.h264
 echo $name
-raspivid -t 0 -n -w 1920 -h 1080 -hf -ih -fl -fps 10 -o - | tee >$name #| nc -k -l 2222
+raspivid -t 0 -n -w 1920 -h 1080 -hf -ih -fl -fps 10 -o - | tee >$name & #| nc -k -l 2222

c0ac8cf1377933ebc1b9bde5068dc46b720f7567
Abhik Chowdhury <abhik.ch6@gmail.com>
2018-09-07 21:22:26 -0700

    fixed file paths for rpi data

--- OverlayRPIAudioCap/Audcap.sh
+++ OverlayRPIAudioCap/Audcap.sh
@@ -2,6 +2,6 @@
 t="$(date +"%d-%m-%Y-%H-%M-%S.%N")"
 productName=$2
 sensorName=$1
-name=$productName-$sensorName-$t-.wav
+name=/home/pi/$productName-$sensorName-$t-.wav
 echo $name
 arecord --device=hw:1,0 --format S16_LE --rate 44100 -c2 $name &
--- OverlayRPIVidCap/Vidcap.sh
+++ OverlayRPIVidCap/Vidcap.sh
@@ -2,6 +2,6 @@
 t="$(date +"%d-%m-%Y-%H-%M-%S.%N")"
 productName=$2
 sensorName=$1
-name=$productName-$sensorName-$t-.h264
+name=/home/pi/$path$productName-$sensorName-$t-.h264
 echo $name
 raspivid -t 0 -n -w 1920 -h 1080 -hf -ih -fl -fps 10 -o - | tee >$name & #| nc -k -l 2222

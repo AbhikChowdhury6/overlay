636384d6ca0cb868baf16d9f4bcceeb2e483a192
Abhik Chowdhury <Abhik.ch6@gmail.com>
2018-09-24 14:33:17 -0700

    I think i got the video recording working and made a test upload script

--- OverlayRPIVidCap/Vidcap.sh
+++ OverlayRPIVidCap/Vidcap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#bash /home/pi/overlay/OverlayRPIVidCap/Vidcap.sh Sensor_Name Product_Name
 t="$(date +"%d-%m-%Y-%H-%M-%S.%N")"
 productName=$2
 sensorName=$1
-port=$3
+
 name=/home/pi/$path$productName-$sensorName-$t-.h264
 echo $name
-raspivid -t 36000000 -ih -fps 10 -o $name &
+raspivid -t 0 -n -w 1920 -h 1080 -ih -fps 10 -o $name &

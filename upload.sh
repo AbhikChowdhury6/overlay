58ae7820a1554986eeccc25090fe9dd9be2a539d
Abhik Chowdhury <Abhik.ch6@gmail.com>
2018-09-24 14:56:33 -0700

    made the upload code work hopefully

--- upload.sh
+++ upload.sh
@@ -1,37 +1,38 @@
 #bash upload.sh chowder test_data 192.168.1.xxx
-IP=$3
+IP=$4
 user=$1
 path=$2
+pass=$3
 
 for filename in *.csv; do
-    echo filename
+    echo $filename
     arrIN=(${filename//-/ })
-    productName=arrIN[0]
-    echo arrIN[0]
-    sensorName=arrIN[1]
-    echo arrIN[1]
-    scp filename $user@$IP:$path/$productName/$sensorName
-    rm filename
+    productName=${arrIN[0]}
+    echo ${arrIN[0]}
+    sensorName=${arrIN[1]}
+    echo ${arrIN[1]}
+    sshpass -p $pass scp $filename $user@$IP:$path/$productName/$sensorName
+    rm $filename
 done
 
 for filename in *.wav; do
-    echo filename
+    echo $filename
     arrIN=(${filename//-/ })
-    productName=arrIN[0]
-    echo arrIN[0]
-    sensorName=arrIN[1]
-    echo arrIN[1]
-    scp filename $user@$IP:$path/$productName/$sensorName
-    rm filename
+    productName=${arrIN[0]}
+    echo ${arrIN[0]}
+    sensorName=${arrIN[1]}
+    echo ${arrIN[1]}
+    sshpass -p $pass scp $filename $user@$IP:$path/$productName/$sensorName
+    rm $filename
 done
 
 for filename in *.h264; do
-    echo filename
+    echo $filename
     arrIN=(${filename//-/ })
-    productName=arrIN[0]
-    echo arrIN[0]
-    sensorName=arrIN[1]
-    echo arrIN[1]
-    scp filename $user@$IP:$path/$productName/$sensorName
-    rm filename
+    productName=${arrIN[0]}
+    echo ${arrIN[0]}
+    sensorName=${arrIN[1]}
+    echo ${arrIN[1]}
+    sshpass -p $pass scp $filename $user@$IP:$path/$productName/$sensorName
+    rm $filename
 done

096260e2726aa67f32d7a0cce8d6c80f7f058693
Abhik Chowdhury <Abhik.ch6@gmail.com>
2018-10-09 15:07:19 -0700

    totally restructured so we can now work properly in teams, this has broken the auto datacollection command on the pi's

--- laptop/app.js
+++
@@ -1,7 +0,0 @@
-var RecordRTC = require('recordrtc');
-
-var options = {
-    recorderType: MediaStreamRecorder,
-    mimeType: 'video/webm\;codecs=h264'
-};
-var recordRTC = RecordRTC(stream, options);
